f6668e373552330cfa147f7b8862bc3a8a79c599
s.soler <s.soler@alumno.um.edu.ar>
2024-06-11 10:46:18 -0300

    subir

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from hola import suma
+
+class TestSuma(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(suma(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
